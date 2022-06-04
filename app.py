1c96d581b7359f3c3e446ae722354ebbbefa26a2
SKILLCATE <98591147+skillcate@users.noreply.github.com>
2022-06-04 21:50:20 +0530

    Add files via upload
    updated web app

--- app.py
+++ app.py
@@ -1,27 +1,45 @@
+# Library imports
+import pandas as pd
+import numpy as np
+import spacy
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+import joblib
+import string
+from spacy.lang.en.stop_words import STOP_WORDS
 from flask import Flask, request, jsonify, render_template
+import nltk
+
+# Load trained Pipeline
+model = joblib.load('sentiment_model.pkl')
+
+stopwords = list(STOP_WORDS)
 
 # Create the app object
 app = Flask(__name__)
 
 
-# importing function for calculations
-from basic_calculator_function import basic_calculator
+# creating a function for data cleaning
+from custom_tokenizer_function import CustomTokenizer
 
-# Define calculator
+
+# Define predict function
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    new_review = [str(x) for x in request.form.values()]
+#     data = pd.DataFrame(new_review)
+#     data.columns = ['new_review']
 
-    a = request.form['a']
-    b = request.form['b']
-    operation = str(request.form['operation'])
-
-    result = basic_calculator(a,b,operation)
-
-    return render_template('index.html', prediction_text=str(result))
+    predictions = model.predict(new_review)[0]
+    if predictions==0:
+        return render_template('index.html', prediction_text='Negative')
+    else:
+        return render_template('index.html', prediction_text='Positive')
 
 
 if __name__ == "__main__":
